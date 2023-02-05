f3e98cec18004a1ea4fbfc6feda350e9c6c1d888
JamlBurk <jamlburk@iu.edu>
2023-02-04 23:58:17 -0500

    Completes Exercise

--- Enemy/Bullet.gd
+++ Enemy/Bullet.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+
+
 var velocity = Vector2.ZERO
 var speed = 500.0
 var damage = 1
@@ -18,15 +20,17 @@ func _physics_process(_delta):
 
 
 func _on_Area2D_body_entered(body):
-	if body.has_method("damage"):
-		body.damage(damage)
-	Effects = get_node_or_null("/root/Game/Effects")
-	if Effects != null:
-		var explosion = Explosion.instance()
-		Effects.add_child(explosion)
-		explosion.global_position = global_position
-	queue_free()
+	if body.name != "Enemy":
+		if body.has_method("damage"):
+			body.damage(damage)
+		Effects = get_node_or_null("/root/Game/Effects")
+		if Effects != null:
+			var explosion = Explosion.instance()
+			Effects.add_child(explosion)
+			explosion.global_position = global_position
+		queue_free()
 
 
 func _on_Timer_timeout():
 	queue_free()
+
