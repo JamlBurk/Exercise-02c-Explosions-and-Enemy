5b8b005f781df6cc701f986f7c3e2741335d9dcb
JamlBurk <jamlburk@iu.edu>
2023-02-05 00:11:59 -0500

    Update Player_Container.gd

--- Player/Player_Container.gd
+++ Player/Player_Container.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-onready var Player = load("res/Player/Player.tscn")
+onready var Player = load("res://Player/Player.tscn")
 
 func _physics_process(_delta):
 	if get_child_count() == 0:
